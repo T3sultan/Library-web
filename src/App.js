4a4c8faa95625f45b9c55ec5417fc48c3132df09
“T3sultan” <incognito.soul011@gmail.com>
2022-12-14 14:05:37 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./views/components/NabBar/Navbar";
+import Login from "./views/pages/auth/Login";
+import Signup from "./views/pages/auth/Signup";
+import MyHome from "./views/pages/myBook/MyBook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  mr-16 ml-16 ">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MyHome />}></Route>
+        <Route path="/home" element={<MyHome />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/views/components/NabBar/Navbar.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+
+const Navbar = () => {
+  const menuItems = (
+    <>
+      <li className="mr-40">
+        <Link to="/home" className=" font-bold text-gray-600">
+          My Books
+        </Link>
+      </li>
+      <li tabindex="0" className=" mr-20 ">
+        <a class="justify-between font-bold text-gray-600">
+          Browse
+          <svg
+            class="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+          </svg>
+        </a>
+        <ul class="p-2">
+          <li>
+            <a>Subject</a>
+          </li>
+          <li>
+            <a>Trending</a>
+          </li>
+        </ul>
+      </li>
+      <div className="mr-30">
+        <div className="form-control  ">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered "
+          />
+        </div>
+      </div>
+      <li>
+        <Link className="font-bold text-gray-600" to="/login">
+          Log In
+        </Link>
+      </li>{" "}
+      <li>
+        <Link className="font-bold text-gray-600" to="/signup">
+          Sign Up
+        </Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar  bg-base-100  px-12">
+      <div className="navbar-start">
+        <div className="dropdown ">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu  menu-compact dropdown-content mt-3 p-2 bg-base-100 rounded-box w-52 "
+          >
+            {menuItems}
+          </ul>
+        </div>
+        <Link to="/home">
+          <img
+            className=" w-2/6 hidden cursor-pointer sm:flex  "
+            src={logo}
+            alt=""
+          />
+        </Link>
+      </div>
+
+      <div className="navbar-end  ">
+        <div className=" hidden navbar-center lg:flex ">
+          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+        </div>
+        <label
+          htmlFor="dashboard-sidebar"
+          tabIndex={1}
+          className="btn btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
